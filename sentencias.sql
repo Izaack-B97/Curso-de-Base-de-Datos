0e6a6f95c1dbcf7dea55d20d72731768cc497178
Isaac Bustamante <izaack.97@gmail.com>
2020-06-22 15:25:35 -0700

    Condicionales

--- sentencias.sql
+++ sentencias.sql
@@ -1,8 +1,14 @@
+DROP DATABASE IF EXISTS libreria_cf;
+
+CREATE DATABASE IF NOT EXISTS libreria_cf;
+
+USE libreria_cf;
+
 /*  ¿Que tipo de entidades? autores
 Nombre: autores 
 */ 
 
-CREATE TABLE autores (
+CREATE TABLE IF NOT EXISTS autores (
     autor_id INT,
     nombre VARCHAR(25),
     apellido VARCHAR(25),
@@ -14,5 +20,21 @@ CREATE TABLE autores (
 --  Crea una tabla a partir de otra
 CREATE TABLE usuarios LIKE autores:
 
+/*
 INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen)
 VALUES (1, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico');
+
+INSERT INTO autores (autor_id, nombre)
+VALUES (1, 'Test Autor');
+
+*/
+-- Multiples Autores
+INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) VALUES 
+(1, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico'),
+(2, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico'),
+(3, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico'),
+(4, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico'),
+(5, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico');
+
+
+SELECT * FROM autores;
