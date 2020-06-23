9700486fd6005a84277714db6cbe9206dd76f480
Isaac Bustamante <izaack.97@gmail.com>
2020-06-22 21:04:59 -0700

    Reinstricciones not null y unique

--- sentencias.sql
+++ sentencias.sql
@@ -9,16 +9,17 @@ Nombre: autores
 */ 
 
 CREATE TABLE IF NOT EXISTS autores (
-    autor_id INT,
-    nombre VARCHAR(25),
-    apellido VARCHAR(25),
-    genero CHAR(1), -- M O F
-    fecha_nacimiento DATE,
-    pais_origen VARCHAR(40)
+    autor_id INT NOT NULL,
+    nombre VARCHAR(25) NOT NULL,
+    apellido VARCHAR(25) NOT NULL,
+    seudonimo VARCHAR(50) UNIQUE,
+    genero CHAR(1) NOT NULL, -- M O F
+    fecha_nacimiento DATE NOT NULL,
+    pais_origen VARCHAR(40) NOT NULL
 );
 
 --  Crea una tabla a partir de otra
-CREATE TABLE usuarios LIKE autores:
+-- CREATE TABLE usuarios LIKE autores;
 
 /*
 INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen)
@@ -36,5 +37,8 @@ INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_
 (4, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico'),
 (5, 'Test Autor', 'Test Autor', 'M', '2018-01-30', 'Mexico');
 
+
+INSERT INTO autores (autor_id, nombre, apellido, seudonimo,genero, fecha_nacimiento, pais_origen) VALUES 
+(6, 'Stephen Edwin', 'King', 'Richard Bachman', 'M', '1947-09-07', 'USA');
 
 SELECT * FROM autores;
