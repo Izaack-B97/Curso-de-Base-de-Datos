92a310956abcd74ed9bda536a36d4de7ccccfa9e
Isaac Bustamante <izaack.97@gmail.com>
2020-06-25 12:56:21 -0700

    Creando funciones personalizadas

---
+++ funciones.sql
@@ -0,0 +1,20 @@
+DELIMITER //
+
+CREATE FUNCTION agregar_dias(fecha DATE, dias INT) RETURNS DATE
+BEGIN
+    RETURN fecha + INTERVAL dias DAY;
+END//
+
+
+CREATE FUNCTION holaMundo() RETURNS VARCHAR(20)
+BEGIN
+    RETURN ‘HolaMundo’;
+END//
+
+DELIMITER ;
+
+-- lISTAR FUNCIONES
+SELECT name FROM mysql.proc WHERE db = database() AND type = 'FUNCTION';
+DROP FUNCTION agregar_dias; -- Elimina una funcion
+
+-- Ejecutar sentencias dentro de funciones
