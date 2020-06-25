6f7228d5880da89a0f923085588a4c402469f7ef
Isaac Bustamante <izaack.97@gmail.com>
2020-06-25 13:04:00 -0700

    Mas funciones

--- funciones.sql
+++ funciones.sql
@@ -18,3 +18,14 @@ SELECT name FROM mysql.proc WHERE db = database() AND type = 'FUNCTION';
 DROP FUNCTION agregar_dias; -- Elimina una funcion
 
 -- Ejecutar sentencias dentro de funciones
+DELIMITER //
+
+CREATE FUNCTION obtener_paginas() RETURNS INT
+BEGIN 
+    SET @paginas = (SELECT (ROUND( RAND() * 100 ) * 4));
+    RETURN @paginas;
+END//
+
+DELIMITER ;
+
+UPDATE libros SET paginas = obtener_paginas();
